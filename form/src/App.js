75ff88f89ee5dbef725c3e3f1a99d644c0f2ad19
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-09-15 21:46:49 -0300

    Doing the CSS

--- form/src/App.js
+++ form/src/App.js
@@ -4,9 +4,9 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Form></Form>
-    </>
+    </div>
   );
 }
 
--- form/src/components/Form/index.jsx
+++ form/src/components/Form/index.jsx
@@ -1,6 +1,7 @@
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import "./style.css";
 
 const Form = () => {
   const formValidator = yup.object().shape({
@@ -41,49 +42,91 @@ const Form = () => {
 
   return (
     <>
-      <h1>Entre para a Kenzie !!</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" {...register("name")} placeholder="Nome Completo" />
-        <p className="inputError">{errors.name?.message}</p>
+      <h1 className="formTitle">Entre para a Kenzie !!</h1>
 
-        <input
-          type="text"
-          {...register("email")}
-          placeholder="Insira seu email"
-        />
-        <p className="inputError">{errors.email?.message}</p>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="formLine">
+          <label className="formLineItem">
+            <input
+              className="input"
+              type="text"
+              {...register("name")}
+              placeholder="Nome Completo"
+            />
+            <p className="inputError">{errors.name?.message}</p>
+          </label>
+          <label className="formLineItem">
+            <input
+              className="input"
+              type="text"
+              {...register("email")}
+              placeholder="Insira seu email"
+            />
+            <p className="inputError">{errors.email?.message}</p>
+          </label>
+        </div>
 
-        <input
-          type="text"
-          {...register("confirmEmail")}
-          placeholder="Confirme seu email"
-        />
-        <p className="inputError">{errors.confirmEmail?.message}</p>
+        <div className="formLine">
+          <label className="formLineItem">
+            <input
+              className="input"
+              type="text"
+              {...register("confirmEmail")}
+              placeholder="Confirme seu email"
+            />
+            <p className="inputError">{errors.confirmEmail?.message}</p>
+          </label>
+          <label className="formLineItem">
+            <input
+              className="input"
+              type="password"
+              {...register("password")}
+              placeholder="Insira sua senha"
+            />
+            <p className="inputError">{errors.password?.message}</p>
+          </label>
+        </div>
+        <div className="formLine">
+          <label className="formLineItem">
+            <input
+              className="input"
+              type="password"
+              {...register("confirmPassword")}
+              placeholder="Confirme sua senha"
+            />
+            <p className="inputError">{errors.confirmPassword?.message}</p>
+          </label>
+          <label className="formLineItem">
+            <input
+              className="input"
+              type="tel"
+              {...register("tel")}
+              placeholder="Insira seu telefone"
+            />
+            <p className="inputError">{errors.tel?.message}</p>
+          </label>
+        </div>
+        <div className="formLine">
+          {" "}
+          <label className="formLineItem">
+            <div>
+              <input
+                className="inputCheckbox"
+                type="checkbox"
+                {...register("terms")}
+              />{" "}
+              <p className="termsText">
+                {" "}
+                Não li mas concordo com os termos de uso{" "}
+              </p>
+            </div>
 
-        <input
-          type="password"
-          {...register("password")}
-          placeholder="Insira sua senha"
-        />
-        <p className="inputError">{errors.password?.message}</p>
-
-        <input
-          type="password"
-          {...register("confirmPassword")}
-          placeholder="Confirme sua senha"
-        />
-        <p className="inputError">{errors.confirmPassword?.message}</p>
-
-        <input type="tel" {...register("tel")} placeholder="Insira" />
-        <p className="inputError">{errors.tel?.message}</p>
-
-        <label htmlFor="">
-          <input type="checkbox" {...register("terms")} /> Não li mas concordo
-          com os termos de uso
-        </label>
-        <p className="inputError">{errors.terms?.message}</p>
-
-        <button type="submit">Entrar para Kenzie !</button>
+            <p className="inputError">{errors.terms?.message}</p>
+          </label>
+          <button className="submitButton" type="submit">
+            Entrar para Kenzie !
+          </button>
+        </div>
       </form>
     </>
   );
